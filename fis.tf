a98d2a100faea3cc72633bd6a581aca4588557c1
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-05-27 23:56:34 +0900

    wip

--- fis.tf
+++ fis.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "fis" {
-  name              = "/${var.service}/fis"
+  name = "/${var.service}/fis"
 }
 
 resource "aws_iam_role" "fis" {
@@ -17,3 +17,42 @@ resource "aws_iam_role" "fis" {
     ]
   })
 }
+
+resource "aws_iam_role_policy_attachment" "fis_ec2" {
+  role       = aws_iam_role.fis.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSFaultInjectionSimulatorEC2Access"
+}
+
+resource "aws_iam_role_policy_attachment" "fis_network" {
+  role       = aws_iam_role.fis.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSFaultInjectionSimulatorNetworkAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "fis_rds" {
+  role       = aws_iam_role.fis.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSFaultInjectionSimulatorRDSAccess"
+}
+
+# resource "aws_iam_policy" "fis_custom" {
+#   name = "${var.service}-fis-custom"
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect   = "Deny"
+#         Action   = "*"
+#         Resource = "*"
+#         Condition = {
+#           "StringNotEqualsIfExists" = {
+#             "aws:ResourceTag/fis/service" = "aws-fis-newbie"
+#           }
+#         }
+#       }
+#     ]
+#   })
+# }
+
+# resource "aws_iam_role_policy_attachment" "fis_custom" {
+#   role       = aws_iam_role.fis.name
+#   policy_arn = aws_iam_policy.fis_custom.arn
+# }
